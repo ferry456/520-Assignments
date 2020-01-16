84575348e7c15797e433496c13854c41aa71763a
ferry456 <ruiy49804@gmail.com>
2020-01-15 23:57:23 -0800

    change

--- hw_1/complex/complex.h
+++ hw_1/complex/complex.h
@@ -3,26 +3,21 @@
 
 /*! @file */
 
-/*! \breif Fraction object and method definitions
- *
- *  A fraction object is a struct with a numerator, denoted num, and
- *  a denominator, denoted den. Varions Methods that take fractions and return 
- *  fractions do arithmetical operations on them.
- */
+
 typedef struct {
     double real;
     double im;
 } Complex;
 
-/*! Add two fractions together
+/*! Add two complexes together
  *  \param a The first summand
  *  \param b The second summand
  */
 Complex add ( Complex a, Complex b );
 
-/*! Multiply two fractions together (does not reduce)
- *  \param a The first term
- *  \param b The second term
+/*! Multiply two complexes together 
+ *  \param a The first complex
+ *  \param b The second complex
  */
 Complex multiply ( Complex a, Complex b );
 
